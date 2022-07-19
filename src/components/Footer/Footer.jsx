15335ccd2a6cf9188bec260c6182abb29810b907
nick3point5 <kenny.trinh4444@gmail.com>
2022-07-19 17:40:19 -0500

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+export function Footer(props) {
+	
+  return (
+		<div className="Footer">
+		<footer className="center">
+			<a href="http://www.kenny-trinh.ga/">
+				Kenny Trinh © 2021            
+			</a>
+		</footer>
+	</div>
+  )
+}
